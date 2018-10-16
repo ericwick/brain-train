e8bfc29cf5147c9351648b298242697f2ed7b0c7
Aftab Baig <aftabbaig@Aftabs-MacBook-Air.local>
2018-10-16 12:45:55 -0500

    housekeeping

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,21 +1,3 @@
-// import React from "react";
-// import { createStackNavigator } from "react-navigation";
-// import LoginScreen from "../screens/LoginScreen";
-// import HomeScreen from "../screens/HomeScreen";
-// import LandingScreen from "../screens/LandingScreen";
-// import MainTabNavigator from './MainTabNavigator';
-
-// const AppNavigator = createStackNavigator({
-//   Home: { screen: HomeScreen },
-//   Login: { screen: LoginScreen },
-//   Landing: { screen: LandingScreen },
-
-// });
-
-// export default AppNavigator;
-
-
-
 import React from 'react';
 import { createSwitchNavigator } from 'react-navigation';
 
@@ -24,5 +6,6 @@ import MainTabNavigator from './MainTabNavigator';
 export default createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+
   Main: MainTabNavigator,
 });
