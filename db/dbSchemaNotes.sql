990b9620173dfb2c94fa4b91770097e5fc15eb8c
anthony62490 <anthonyjmagno@gmail.com>
2018-10-15 23:57:16 -0500

    updated schema

--- db/dbSchemaNotes.sql
+++ db/dbSchemaNotes.sql
@@ -8,6 +8,7 @@ CREATE TABLE users
 
 CREATE TABLE global_stats
 ( 
+  stat_index SERIAL PRIMARY KEY,
   uid INTEGER NOT NULL,
   gid INTEGER NOT NULL,
   time_start TIMESTAMP NOT NULL, --Must be supplied by the post script
@@ -29,7 +30,12 @@ VALUES ('user', 'password');
 
 INSERT INTO global_stats (uid, gid, time_start, time_end, score)
 VALUES(1, 1, '2018-10-15T15:20:19.109624Z', '2018-10-15T15:49:55.058379Z', 2500),
-      (1, 2, '2018-10-15T15:49:55.058379Z', '2018-10-15T16:06:41.2315Z', 5300);
+      (1, 2, '2018-10-15T15:49:55.058379Z', '2018-10-15T16:06:41.2315Z', 5300),
+      (1, 1, '2018-10-15T20:20:19.109Z', '2018-10-15T20:49:55.058Z', 2500),
+      (1, 2, '2018-10-15T20:49:55.058Z', '2018-10-15T21:06:41.231Z', 5300),
+      (2, 1, '2018-10-16T02:25:53.860Z', '2018-10-16T02:38:12.120Z', 2600),
+      (3, 2, '2018-10-16T07:38:12.120Z', '2018-10-16T02:39:46.995Z', 3200),
+      (3, 2, '2018-10-16T08:43:30.661Z', '2018-10-16T08:44:06.020Z', 1200);
 
 INSERT INTO global_games (game_name, game_cat, game_desc)
 VALUES('Make Me a Match', 'memory', 'Flip over cards to make pairs. Get as many matches in as few moves as possible.'),
